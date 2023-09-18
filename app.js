adcf899cfe1b07f37e1015e7a5571ccb249be23e
TTundeson <141639027+TTundeson@users.noreply.github.com>
2023-09-18 21:50:15 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let currentElement = arr[i];
+    let j = i - 1;
+
+    while (j >= 0 && arr[j] > currentElement) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+
+    arr[j + 1] = currentElement;
+  }
+}
+
+// Example usage:
+let myArray = [12, 11, 13, 5, 6];
+insertionSort(myArray);
+console.log('Sorted array:', myArray);
